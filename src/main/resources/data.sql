d10dbd287a9b71aa23ae9237cf2c65805a0a9288
Gustavo correa <gustavocorrea.1910@gmail.com>
2026-06-15 13:25:01 -0300

    inserindo os times no banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,57 @@
+-- América do Sul
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Brasil', 'https://flagcdn.com/w160/br.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Argentina', 'https://flagcdn.com/w160/ar.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Uruguai', 'https://flagcdn.com/w160/uy.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Colômbia', 'https://flagcdn.com/w160/co.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Equador', 'https://flagcdn.com/w160/ec.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Peru', 'https://flagcdn.com/w160/pe.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Paraguai', 'https://flagcdn.com/w160/py.png');
+
+-- Europa
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('França', 'https://flagcdn.com/w160/fr.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Alemanha', 'https://flagcdn.com/w160/de.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Espanha', 'https://flagcdn.com/w160/es.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Inglaterra', 'https://flagcdn.com/w160/gb-eng.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Portugal', 'https://flagcdn.com/w160/pt.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Itália', 'https://flagcdn.com/w160/it.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Holanda', 'https://flagcdn.com/w160/nl.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Bélgica', 'https://flagcdn.com/w160/be.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Croácia', 'https://flagcdn.com/w160/hr.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Suíça', 'https://flagcdn.com/w160/ch.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Dinamarca', 'https://flagcdn.com/w160/dk.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Sérvia', 'https://flagcdn.com/w160/rs.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Polônia', 'https://flagcdn.com/w160/pl.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Suécia', 'https://flagcdn.com/w160/se.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Gales', 'https://flagcdn.com/w160/gb-wls.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Ucrânia', 'https://flagcdn.com/w160/ua.png');
+
+-- América Central e Norte
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('EUA', 'https://flagcdn.com/w160/us.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('México', 'https://flagcdn.com/w160/mx.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Canadá', 'https://flagcdn.com/w160/ca.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Costa Rica', 'https://flagcdn.com/w160/cr.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Panamá', 'https://flagcdn.com/w160/pa.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Jamaica', 'https://flagcdn.com/w160/jm.png');
+
+-- África
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Senegal', 'https://flagcdn.com/w160/sn.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Marrocos', 'https://flagcdn.com/w160/ma.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Egito', 'https://flagcdn.com/w160/eg.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Nigéria', 'https://flagcdn.com/w160/ng.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Camarões', 'https://flagcdn.com/w160/cm.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Gana', 'https://flagcdn.com/w160/gh.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Argélia', 'https://flagcdn.com/w160/dz.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('C. do Marfim', 'https://flagcdn.com/w160/ci.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Tunísia', 'https://flagcdn.com/w160/tn.png');
+
+-- Ásia e Oceania
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Japão', 'https://flagcdn.com/w160/jp.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Coreia do Sul', 'https://flagcdn.com/w160/kr.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Irã', 'https://flagcdn.com/w160/ir.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Arábia Saudita', 'https://flagcdn.com/w160/sa.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Austrália', 'https://flagcdn.com/w160/au.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Catar', 'https://flagcdn.com/w160/qa.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('EAU', 'https://flagcdn.com/w160/ae.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Iraque', 'https://flagcdn.com/w160/iq.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Uzbequistão', 'https://flagcdn.com/w160/uz.png');
+INSERT INTO TB_TIMES (NOME, URL_BANDEIRA) VALUES ('Nova Zelândia', 'https://flagcdn.com/w160/nz.png');
